f90554b430704b1071fb3b85c11db98ed29797a0
tiagolisboanovais <tiagolisboanovais@outlook.com>
2022-12-05 06:47:30 -0300

    Arquivo com script que chama processo de atualização que tem com base as planilha de edição em massa da shopee.

---
+++ modulos/shopee.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+def importa_informacoes_basicas():
+    print('Realizando importação das planilhas da shopee para atualização.')
+    contagem_inicial = 0
+    dir = "planilhas_atualizar/Informacoes_basicas"
+    for path in os.listdir(dir):
+        if os.path.isfile(os.path.join(dir, path)):
+            contagem_inicial += 1
+    print(f'Foram identificadas '+str(contagem_inicial)+' planilhas a serem atualizadas.')
+    print('Realizando conexão com as planilhas.')
+    print('Isto pode levar alguns minutos, por favor aguarde...')
+    print('Conexão com a planilha realizada com sucesso.')
